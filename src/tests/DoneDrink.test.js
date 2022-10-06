a4e9bbfbd3e5ea28300bd1ef7f3968ac149ae59b
Reinaldo <reinaldoper83@gmail.com>
2022-10-05 21:20:33 -0300

    testes revenue

--- src/tests/DoneDrink.test.js
+++ src/tests/DoneDrink.test.js
@@ -12,6 +12,7 @@ describe('Testand done drinks', () => {
     id: '14610',
     type: 'drink',
     category: 'Shot',
+    doneDate: '05/09/2022',
     name: 'ACID',
     image: 'https://www.thecocktaildb.com/images/media/drink/xuxpxt1479209317.jpg',
   },
@@ -22,14 +23,16 @@ describe('Testand done drinks', () => {
     localStorage.setItem(mockFavorites, JSON.stringify(listFavorites));
     const { history } = renderWithRouterAndRedux(<App />);
     history.push('/done-recipes');
-    /* expect(getItem).toHaveBeenCalled(); */
+    /* localStorage.setItem(mockFavorites, JSON.stringify(listFavorites));
+    expect(localStorage.getItem(mockFavorites)).toEqual(JSON.stringify(listFavorites)); */
     const btn = screen.getByTestId('filter-by-drink-btn');
     expect(btn).toBeInTheDocument();
     userEvent.click(btn);
     await waitFor(() => {
       const share = screen.queryAllByTestId('0-horizontal-share-btn');
       expect(share[0]).toBeInTheDocument();
-
+      const date = screen.getByText('05/09/2022');
+      expect(date).toBeInTheDocument();
       window.document.execCommand = jest.fn().mockImplementation(() => ' ');
       userEvent.click(share[0]);
       const linkCopied = screen.getByText('Link copied!');
