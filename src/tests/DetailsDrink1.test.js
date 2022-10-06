80ad12f4964ee8a5abe99afa9f994c62bdf1fbde
Reinaldo <reinaldoper83@gmail.com>
2022-10-06 11:30:29 -0300

    ajeitando improgress

---
+++ src/tests/DetailsDrink1.test.js
@@ -0,0 +1,56 @@
+import { screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import App from '../App';
+/* import DrinksPage from '../pages/DrinksPage'; */
+/* import MealsPage from '../pages/MealRecipe'; */
+import GG from './helpers/GG';
+import meals from './helpers/meals';
+import renderWithRouterAndRedux from './helpers/rendeWithRouterAndRedux';
+
+const mockFetch = (data) => Promise.resolve({
+  json: () => Promise.resolve(data),
+});
+
+const flushPromises = () => new Promise((r) => { setTimeout(r); });
+
+describe('Testand details Meals', () => {
+  beforeEach(() => {
+    const mockMultFetch = jest.fn()
+      .mockReturnValueOnce(mockFetch(GG))
+      .mockReturnValueOnce(mockFetch(meals));
+    global.fetch = mockMultFetch;
+  });
+  const mockFavorites = 'favoriteRecipes';
+  const listFavorites = [{
+    id: '15997',
+    /* nationality: 'Turkish', */
+    name: 'GG',
+    image: 'https://www.thecocktaildb.com/images/media/drink/vyxwut1468875960.jpg',
+  }];
+  const setLocalStorage = (id, data) => {
+    window.localStorage.setItem(id, JSON.stringify(data));
+  };
+  setLocalStorage(mockFavorites, listFavorites);
+  test('Drinks details', async () => {
+    await flushPromises();
+    expect(localStorage.getItem(mockFavorites)).toEqual(JSON.stringify(listFavorites));
+    const { history } = renderWithRouterAndRedux(<App />);
+    history.push('/drinks/15997');
+
+    await waitFor(() => expect(global.fetch).toHaveBeenCalled());
+    expect(screen.getByText('GG')).toBeInTheDocument();
+
+    const photo = screen.getByTestId('recipe-photo');
+    expect(photo).toBeInTheDocument();
+    const recomendation = screen.getByTestId('0-recommendation-card');
+    expect(recomendation).toBeInTheDocument();
+    const btnFavorite = screen.getByTestId('favorite-btn');
+    expect(btnFavorite).toBeInTheDocument();
+    userEvent.click(btnFavorite);
+    setLocalStorage(mockFavorites, listFavorites);
+    expect(localStorage.getItem(mockFavorites)).toEqual(JSON.stringify(listFavorites));
+
+    global.fetch.mockClear();
+  });
+});
---
+++ src/tests/ImprogressMeals.test.js
@@ -0,0 +1,55 @@
+import { screen, waitFor } from '@testing-library/react';
+/* import userEvent from '@testing-library/user-event'; */
+import React from 'react';
+import App from '../App';
+/* import DrinksPage from '../pages/DrinksPage'; */
+/* import MealsPage from '../pages/MealRecipe'; */
+import Corba from './helpers/Corba';
+/* import drinks from './helpers/drinks'; */
+import renderWithRouterAndRedux from './helpers/rendeWithRouterAndRedux';
+
+const mockFetch = (data) => Promise.resolve({
+  json: () => Promise.resolve(data),
+});
+
+const flushPromises = () => new Promise((r) => { setTimeout(r); });
+beforeEach(() => {
+  const mockMultFetch = jest.fn()
+    .mockReturnValueOnce(mockFetch(Corba));
+  global.fetch = mockMultFetch;
+});
+const mockFavorites = 'inProgressRecipes';
+const listFavorites = [{
+  id: '52977',
+  recipesFinish: [],
+}];
+const setLocalStorage = (id, data) => {
+  window.localStorage.setItem(id, JSON.stringify(data));
+};
+describe('Improgress ingredients', () => {
+  /* const phase = 'phrase-content'; */
+  /* const finisClass = 'finish phrase-content'; */
+
+  test('Improgress Ingredients', async () => {
+    await flushPromises();
+    localStorage.removeItem(mockFavorites);
+    const { history } = renderWithRouterAndRedux(<App />);
+    history.push('meals/52977/in-progress');
+
+    await waitFor(() => expect(global.fetch).toHaveBeenCalled());
+    setLocalStorage(mockFavorites, listFavorites);
+    expect(localStorage.getItem(mockFavorites)).toEqual(JSON.stringify(listFavorites));
+    expect(screen.getByText('Corba')).toBeInTheDocument();
+
+    const recipe = screen.getByTestId('recipe-category');
+    expect(recipe).toBeInTheDocument();
+    const step1 = screen.getByTestId('0-step');
+    expect(step1).toBeInTheDocument();
+    const label = screen.getByTestId('0-ingredient-step');
+    expect(label).toBeInTheDocument();
+
+    /* fireEvent.click(step1);
+    expect(localStorage.getItem(mockFavorites)).toEqual(JSON.stringify(listFavorites)); */
+    global.fetch.mockClear();
+  });
+});
